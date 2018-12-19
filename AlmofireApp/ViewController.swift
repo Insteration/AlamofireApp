9093c396d1e17c61f3197878e80aa791b11780b7
Artem Karmaz <inste@icloud.com>
2018-12-19 22:23:44 +0200

    added frameworks

--- AlmofireApp/ViewController.swift
+++ AlmofireApp/ViewController.swift
@@ -7,14 +7,41 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        AF.request("https://jsonplaceholder.typicode.com/todos/1").responseJSON {response in
+            //            self.str = String(data: response.data!, encoding: String.Encoding.utf8) ?? "str"
+            //            self.label.text = self.str
+            print(response)
+//            print("AlamofireVersionNumber: ", AlamofireVersionNumber)
+            
+            //            var json: Any
+            
+//            do {
+//                let json = try JSONSerialization.jsonObject(with: response.data!, options: [])
+//                print("json: ", json)
+//
+//
+//                guard let jsonArray = json as? [String: Any] else {
+//                    return
+//                }
+//                print("jsonArray: ", jsonArray)
+//
+////                for item in jsonArray{
+//                    //                    self.labelJSON.text = self.labelJSON.text ?? " " + "\n" + item.key + ": " + (item.value as! String)
+//                }
+//
+//            } catch {
+//                print("error parsing")
+//            }
+//
+            
+            
+        }
     }
-
-
 }
 
